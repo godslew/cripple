440aab9a2934de51b666c97fffa48c3db7b9b69d
snake00 <maousnake00@live.jp>
2019-08-25 22:59:56 +0900

    editTextから文字列を取得

--- app/src/main/kotlin/com/godslew/cripple/presenter/tweet/TweetFragment.kt
+++ app/src/main/kotlin/com/godslew/cripple/presenter/tweet/TweetFragment.kt
@@ -2,10 +2,13 @@ package com.godslew.cripple.presenter.tweet
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.toSpannable
 import com.godslew.cripple.R
 import com.godslew.cripple.databinding.FragmentTweetBinding
 
@@ -16,8 +19,14 @@ class TweetFragment : Fragment() {
     fun newInstance() = TweetFragment()
   }
 
+  interface CustomTextWatcher: TextWatcher{
+    override fun beforeTextChanged(c: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+    override fun onTextChanged(c: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+  }
+
   private lateinit var viewModel: TweetViewModel
   private lateinit var binding: FragmentTweetBinding
+  private lateinit var text: String
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
@@ -29,12 +38,32 @@ class TweetFragment : Fragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+
+    binding.buttonTweet.isEnabled = false
     binding.buttonTweet.setOnClickListener {
+      viewModel.postTweet(requireContext(), text)
       activity?.finish()
     }
     binding.closeTweet.setOnClickListener {
       activity?.finish()
     }
+
+    binding.textTweet.addTextChangedListener((object: CustomTextWatcher{
+      override fun afterTextChanged(e: Editable?) {
+        //ここに処理を書く
+        val edit = e ?: return
+        if (edit.isEmpty()) {
+          binding.buttonTweet.isEnabled = false
+          return
+        }
+        if (edit.length > 140) {
+          binding.buttonTweet.isEnabled = false
+          return
+        }
+        binding.buttonTweet.isEnabled = true
+        text = edit.toSpannable().toString()
+      }
+    }))
   }
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/kotlin/com/godslew/cripple/presenter/tweet/TweetViewModel.kt
+++ app/src/main/kotlin/com/godslew/cripple/presenter/tweet/TweetViewModel.kt
@@ -1,7 +1,17 @@
 package com.godslew.cripple.presenter.tweet
 
+import android.content.Context
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
 
 class TweetViewModel : ViewModel() {
-  // TODO: Implement the ViewModel
+  private val disposables = CompositeDisposable()
+
+  override fun onCleared() {
+    disposables.dispose()
+  }
+  fun postTweet(context: Context, text: String) {
+      Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+  }
 }
