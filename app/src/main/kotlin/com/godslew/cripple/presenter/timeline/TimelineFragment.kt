41f49ef943d4fcb82c407f1f3d5f4c14d5895840
snake00 <maousnake00@live.jp>
2019-09-11 15:53:05 +0900

    skeleton ui

--- app/src/main/kotlin/com/godslew/cripple/presenter/timeline/TimelineFragment.kt
+++ app/src/main/kotlin/com/godslew/cripple/presenter/timeline/TimelineFragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.ethanhua.skeleton.Skeleton
 import com.godslew.cripple.R
+import com.godslew.cripple.databinding.TimelineFragmentBinding
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
 
 
 class TimelineFragment : Fragment() {
@@ -16,18 +20,31 @@ class TimelineFragment : Fragment() {
   }
 
   private lateinit var viewModel: TimelineViewModel
+  private lateinit var binding: TimelineFragmentBinding
+  private lateinit var adapter : GroupAdapter<ViewHolder>
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    return inflater.inflate(R.layout.timeline_fragment, container, false)
+    binding = TimelineFragmentBinding.inflate(inflater, container, false)
+    adapter = GroupAdapter()
+    return binding.root
   }
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
     viewModel = ViewModelProviders.of(this).get(TimelineViewModel::class.java)
     // TODO: Use the ViewModel
+    binding.pager.adapter = adapter
+    val skeletonScreen = Skeleton.bind(binding.pager)
+      .shimmer(true)
+      .angle(20)
+      .duration(1200)
+      .load(R.layout.item_skeleton_tweet)
+      .show() //default count is 10
+    binding.pager.postDelayed({ skeletonScreen.hide() }, 3000)
+    return
   }
 
 }
