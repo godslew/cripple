8c6cdf2c6dab4be5ca497c58e6d3f1693b851ae4
snake00 <maousnake00@live.jp>
2019-09-16 15:38:32 +0900

    update dependency

--- app/src/main/kotlin/com/godslew/cripple/presenter/timeline/TimelineFragment.kt
+++ app/src/main/kotlin/com/godslew/cripple/presenter/timeline/TimelineFragment.kt
@@ -37,13 +37,13 @@ class TimelineFragment : Fragment() {
     viewModel = ViewModelProviders.of(this).get(TimelineViewModel::class.java)
     // TODO: Use the ViewModel
     binding.pager.adapter = adapter
-    val skeletonScreen = Skeleton.bind(binding.pager)
-      .shimmer(true)
-      .angle(20)
-      .duration(1200)
-      .load(R.layout.item_skeleton_tweet)
-      .show() //default count is 10
-    binding.pager.postDelayed({ skeletonScreen.hide() }, 3000)
+   // val skeletonScreen = Skeleton.bind(binding.pager)
+   //   .shimmer(true)
+   //   .angle(20)
+   //   .duration(1200)
+   //   .load(R.layout.item_skeleton_tweet)
+   //   .show() //default count is 10
+   // binding.pager.postDelayed({ skeletonScreen.hide() }, 3000)
     return
   }
 
