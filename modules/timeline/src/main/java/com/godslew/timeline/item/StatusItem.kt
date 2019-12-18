af754c14e12b48ad4511270596709a6dba97cdda
snake00 <maousnake00@live.jp>
2019-12-18 19:14:13 +0900

    差分更新を定義

--- modules/timeline/src/main/java/com/godslew/timeline/item/StatusItem.kt
+++ modules/timeline/src/main/java/com/godslew/timeline/item/StatusItem.kt
@@ -2,6 +2,7 @@ package com.godslew.timeline.item
 
 import com.godslew.timeline.R
 import com.godslew.timeline.databinding.ItemStatusTypeTweetBinding
+import com.xwray.groupie.Item
 import com.xwray.groupie.databinding.BindableItem
 
 sealed class StatusItem {
@@ -17,6 +18,24 @@ sealed class StatusItem {
         screenName.text = handleName//"@${status.user.screenName}"
         statusText.text = handleName //status.text
       }
+    }
+
+    override fun isSameAs(other: Item<*>?): Boolean {
+      if (other != null && other is TweetItem) {
+        return other.handleName == handleName
+      }
+      return false
+    }
+
+    override fun equals(other: Any?): Boolean {
+      if (other != null && other is TweetItem) {
+        return other.handleName == handleName
+      }
+      return false
+    }
+
+    override fun hashCode(): Int {
+      return handleName.hashCode()
     }
   }
 }
