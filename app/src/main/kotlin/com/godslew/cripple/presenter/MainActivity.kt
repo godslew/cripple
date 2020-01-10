f03dc5c51f2b33d9ddfa386bc282562558cf5fc6
snake00 <maousnake00@live.jp>
2020-01-10 15:23:22 +0900

    MainActivityにViewModelFactoryを使用

--- app/src/main/kotlin/com/godslew/cripple/presenter/MainActivity.kt
+++ app/src/main/kotlin/com/godslew/cripple/presenter/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.godslew.cripple.presenter
 
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
+import androidx.activity.viewModels
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.godslew.core.android.factory.ViewModelFactory
 import com.godslew.cripple.R
 import com.godslew.cripple.databinding.ActivityMainBinding
 import com.godslew.core.domain.usecase.LoginUseCase
@@ -13,12 +14,16 @@ import com.godslew.gksettingpreferences.SettingPreferences
 import javax.inject.Inject
 
 class MainActivity : BaseActivity() {
-  private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
   @Inject
   lateinit var loginUseCase: LoginUseCase
   @Inject
   lateinit var settingPreferences: SettingPreferences
+  @Inject
+  internal lateinit var factory: ViewModelFactory<MainActivityViewModel>
+
+  private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+  private val viewModel: MainActivityViewModel by viewModels { factory }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
