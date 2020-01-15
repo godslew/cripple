7822df07a91574c2f1f585a7ba12eaf8cf6e4434
snake00 <maousnake00@live.jp>
2020-01-15 16:32:27 +0900

    rxで包む

--- core-domain/src/main/java/com/godslew/core/domain/usecase/TwitterUseCase.kt
+++ core-domain/src/main/java/com/godslew/core/domain/usecase/TwitterUseCase.kt
@@ -5,17 +5,30 @@ import com.godslew.core.java.entity.CrippleStatus
 import io.reactivex.Single
 import twitter4j.Twitter
 import twitter4j.TwitterException
+import twitter4j.User
 import javax.inject.Inject
 
 @SessionScope
 class TwitterUseCase @Inject constructor(
   private val twitter : Twitter
 ) {
-  fun postTweet(text : String) : Single<CrippleStatus> {
+  fun updateStatus(text : String) : Single<CrippleStatus> {
     return Single.create {
       try {
         val status = twitter.updateStatus(text)
         it.onSuccess(CrippleStatus(status, status.user.id))
+      } catch (e : TwitterException) {
+        it.onError(e)
+        e.printStackTrace()
+      }
+    }
+  }
+
+  fun verifyCredentials() : Single<User> {
+    return Single.create {
+      try {
+        val user = twitter.verifyCredentials()
+        it.onSuccess(user)
       } catch (e : TwitterException) {
         it.onError(e)
         e.printStackTrace()
--- modules/tweet/src/main/java/com/godslew/tweet/TweetViewModel.kt
+++ modules/tweet/src/main/java/com/godslew/tweet/TweetViewModel.kt
@@ -4,17 +4,22 @@ import android.app.Application
 import android.content.Context
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
 import com.godslew.core.android.action.AppAction
+import com.godslew.core.android.extensions.bindTo
 import com.godslew.core.android.redux.AppReducer
 import com.godslew.core.android.state.AppState
 import com.godslew.core.android.store.AppStore
+import com.godslew.core.domain.usecase.TwitterUseCase
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class TweetViewModel @Inject constructor(
   application: Application,
-  appStore: AppStore<AppState, AppAction, AppReducer>
+  appStore: AppStore<AppState, AppAction, AppReducer>,
+  private val twitterUseCase: TwitterUseCase
 ) : AndroidViewModel(application) {
   private val disposables = CompositeDisposable()
 
@@ -23,5 +28,10 @@ class TweetViewModel @Inject constructor(
   }
   fun postTweet(context: Context, text: String) {
       Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    twitterUseCase.verifyCredentials()
+      .observeOn(AndroidSchedulers.mainThread())
+      .subscribeOn(Schedulers.newThread())
+      .bindTo {
+      }.addTo(disposables)
   }
 }
