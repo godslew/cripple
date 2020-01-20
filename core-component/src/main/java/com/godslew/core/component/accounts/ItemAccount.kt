fe81bdf9e943966fba2aab280f88c719dc3589af
snake00 <maousnake00@live.jp>
2020-01-20 14:47:35 +0900

    add accountItem

---
+++ core-component/src/main/java/com/godslew/core/component/accounts/ItemAccount.kt
@@ -0,0 +1,37 @@
+package com.godslew.core.component.accounts
+
+import com.godslew.core.component.R
+import com.godslew.core.component.databinding.ItemAccountBinding
+import com.godslew.core.java.entity.Account
+import com.xwray.groupie.Item
+import com.xwray.groupie.databinding.BindableItem
+
+class ItemAccount(
+  val account: Account
+) : BindableItem<ItemAccountBinding>()  {
+  override fun getLayout(): Int = R.layout.item_account
+
+  override fun bind(viewBinding: ItemAccountBinding, position: Int) {
+
+  }
+
+  override fun isSameAs(other: Item<*>?): Boolean {
+    return if (other != null && other is ItemAccount) {
+      other.account.userId() == account.userId()
+    } else {
+      false
+    }
+  }
+
+  override fun equals(other: Any?): Boolean {
+    return if (other != null && other is ItemAccount) {
+      other.account == account
+    } else {
+      false
+    }
+  }
+
+  override fun hashCode(): Int {
+    return account.hashCode()
+  }
+}
