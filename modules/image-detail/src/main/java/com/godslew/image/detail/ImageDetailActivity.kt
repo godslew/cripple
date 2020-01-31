489116d53635d580fb1cb22324f2016d9464f919
snake00 <maousnake00@live.jp>
2020-01-31 19:11:30 +0900

    image-detail, status-detail

---
+++ modules/image-detail/src/main/java/com/godslew/image/detail/ImageDetailActivity.kt
@@ -0,0 +1,6 @@
+package com.godslew.image.detail
+
+import com.godslew.core.android.presenter.BaseActivity
+
+class ImageDetailActivity : BaseActivity() {
+}
---
+++ modules/status-detail/src/main/java/com/godslew/status/detail/StatusDetailActivity.kt
@@ -0,0 +1,6 @@
+package com.godslew.status.detail
+
+import com.godslew.core.android.presenter.BaseActivity
+
+class StatusDetailActivity : BaseActivity() {
+}
