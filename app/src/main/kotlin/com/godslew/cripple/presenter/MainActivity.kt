cccda64664b84fa9f5c5d7693adaf947c92e318b
godslew <maousnake00@live.jp>
2020-09-24 17:39:20 +0900

    delete bottom navigation to become simple

--- app/src/main/kotlin/com/godslew/cripple/presenter/MainActivity.kt
+++ app/src/main/kotlin/com/godslew/cripple/presenter/MainActivity.kt
@@ -1,33 +1,35 @@
 package com.godslew.cripple.presenter
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
-import androidx.core.view.isVisible
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.godslew.core.android.action.AppAction
 import com.godslew.core.android.extensions.bindTo
 import com.godslew.core.android.factory.ViewModelFactory
-import com.godslew.cripple.R
-import com.godslew.cripple.databinding.ActivityMainBinding
 import com.godslew.core.android.presenter.BaseActivity
 import com.godslew.core.android.redux.AppDispatcher
 import com.godslew.core.java.Constants
-import com.godslew.tweet.TweetActivity
+import com.godslew.cripple.R
+import com.godslew.cripple.databinding.ActivityMainBinding
 import com.godslew.gksettingpreferences.SettingPreferences
 import com.godslew.oauth.TwitterLoginActivity
+import com.godslew.timeline.TimelineFragment
+import com.godslew.tweet.TweetActivity
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import javax.inject.Inject
 
+
 class MainActivity : BaseActivity() {
 
   @Inject
-  lateinit var settingPreferences: SettingPreferences
+  internal lateinit var settingPreferences: SettingPreferences
+
   @Inject
   internal lateinit var factory: ViewModelFactory<MainActivityViewModel>
 
-  private val disposable  = CompositeDisposable()
+  private val disposable = CompositeDisposable()
 
   private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
   private val viewModel: MainActivityViewModel by viewModels { factory }
@@ -40,12 +42,6 @@ class MainActivity : BaseActivity() {
     binding.fab.setOnClickListener {
       startActivity(TweetActivity.createIntent(this))
     }
-    val navController = findNavController(R.id.nav_host_fragment)
-    setupWithNavController(binding.bottomNavigation, navController)
-    navController.addOnDestinationChangedListener { _, destination, _ ->
-      binding.fab.isVisible = destination.label == "TimelineFragment"
-    }
-
     setup()
   }
 
@@ -54,7 +50,24 @@ class MainActivity : BaseActivity() {
     super.onDestroy()
   }
 
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    menuInflater.inflate(R.menu.item_menu, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when (item.itemId) {
+      R.id.menu_account -> {
+      }
+      R.id.menu_setting -> {
+
+      }
+    }
+    return true
+  }
+
   private fun setup() {
+    setupTimeline()
     viewModel.loadedAccounts
       .bindTo {
         if (it.isNotEmpty()) {
@@ -66,8 +79,12 @@ class MainActivity : BaseActivity() {
             Constants.RequestCodes.TwitterLogin
           )
         }
-    }.addTo(disposable)
+      }.addTo(disposable)
 
     viewModel.setup()
+  }
+
+  private fun setupTimeline() {
+    supportFragmentManager.beginTransaction().add(R.id.fragment, TimelineFragment()).commit()
   }
 }
